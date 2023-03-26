63cc2089ce37e10e49d57f307ddd9b6385e8cea5
alaidinens <alexey.laidinen@gmail.com>
2023-03-26 23:06:21 +0300

    s

--- insert.php
+++ insert.php
@@ -1,14 +1,12 @@
 <?php
-$klases_nr =$_POST['klases_nr'];
-$laiks =$_POST['laiks'];
-$datums =$_POST['datums'];
-$skolens =$_POST['skolens'];
-$skolotaja =$_POST['skolotaja'];
-$klase =$_POST['klase'];
-$termins =$_POST['termins'];
-?>
 
-if (!empty($klases_nr) || (!empty($laiks) || (!empty($datums) || (!empty($skolens) || (!empty($skolotaja) || (!empty($klase) || (!empty($termins)) {
+$skol_vards =$_POST['skol_vards'];
+$skol_uzvards =$_POST['skol_uzvards'];
+$klases_nr =$_POST['klases_nr'];
+$skolotaja =$_POST['skolotaja'];
+
+
+if (!empty($skol_vards || (!empty($skol_uzvards) || (!empty($klases_nr) || (!empty($skolotaja)) {
   $host = "localhost";
   $dbUsername = "root";
   $dbPassword = "Lolesha161204";
@@ -20,9 +18,11 @@ if (!empty($klases_nr) || (!empty($laiks) || (!empty($datums) || (!empty($skolen
     die('Connect Error('.mysqli_connect_error().')'.mysqli_connect_error());
   } else {
       $select = "select"
-      $INSERT = "INSERT Into register (klases_nr, laiks, datums, skolens, skolotaja, klase, terimins) values (?, ?, ?, ?, ?, ?, ?)";
+      $INSERT = "INSERT Into register (skol_vards, skol_uzvards, klases_nr, skolotaja) values (?, ?, ?, ?)";
   }
 } else {
     echo "All fields are required"
     die();
 }
+
+?>
